119226e15f18787f3d1fc3fc7a866a07ccb218a9
AugustoCorbalan <auguscorbi@gmail.com>
2023-10-09 17:51:25 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const router = require('./routes/index');
+const cors = require('cors');
 const app = express();
+
+const corsOptions={
+    origin: 'http://localhost:3000',
+    methods: 'GET, PUT, DELETE, POST',
+    credentials: true
+}
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
